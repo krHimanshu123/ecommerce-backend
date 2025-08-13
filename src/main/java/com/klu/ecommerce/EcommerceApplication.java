2093ead2a846b37b38431d11f5ea0c5b9756cea8
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-08-13 23:29:49 +0530

    Update EcommerceApplication.java

--- src/main/java/com/klu/ecommerce/EcommerceApplication.java
+++ src/main/java/com/klu/ecommerce/EcommerceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
+
 @SpringBootApplication
 public class EcommerceApplication extends SpringBootServletInitializer {
 
