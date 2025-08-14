773fb9e92f326fd8bc2e661c8bc462d38b99a624
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-08-14 09:41:48 +0530

    Update UserDetailsServiceImpl.java

--- src/main/java/com/klu/ecommerce/security/UserDetailsServiceImpl.java
+++ src/main/java/com/klu/ecommerce/security/UserDetailsServiceImpl.java
@@ -14,6 +14,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
         this.userRepository = userRepository;
     }
 
+    
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username)
