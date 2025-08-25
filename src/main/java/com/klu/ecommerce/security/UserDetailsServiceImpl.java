c52e8caa4cfb9529269f56e334b074fe078a158b
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-08-25 23:51:30 +0530

    Remove unnecessary whitespace in UserDetailsServiceImpl

--- src/main/java/com/klu/ecommerce/security/UserDetailsServiceImpl.java
+++ src/main/java/com/klu/ecommerce/security/UserDetailsServiceImpl.java
@@ -5,7 +5,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
