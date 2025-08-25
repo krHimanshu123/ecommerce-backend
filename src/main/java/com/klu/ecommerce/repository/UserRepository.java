4365d47578a3e28ed2298e98ec28922bb0f5581b
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-08-25 23:50:48 +0530

    Update UserRepository.java

--- src/main/java/com/klu/ecommerce/repository/UserRepository.java
+++ src/main/java/com/klu/ecommerce/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.klu.ecommerce.repository;
 
+
 import com.klu.ecommerce.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
