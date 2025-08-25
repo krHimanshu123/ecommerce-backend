8a6a90cc3feec275702efda84a884db11db44341
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-08-25 23:48:41 +0530

    Update AuthController.java

--- src/main/java/com/klu/ecommerce/controller/AuthController.java
+++ src/main/java/com/klu/ecommerce/controller/AuthController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
-
 @RestController
 @RequestMapping("/auth")
 @CrossOrigin(origins = "*")
