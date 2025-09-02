6940103d2343b73b9bf8da2cd698870fa0d68323
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-09-02 22:47:30 +0530

    Update SecurityConfig.java

--- src/main/java/com/klu/ecommerce/security/SecurityConfig.java
+++ src/main/java/com/klu/ecommerce/security/SecurityConfig.java
@@ -1,5 +1,4 @@
 package com.klu.ecommerce.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
