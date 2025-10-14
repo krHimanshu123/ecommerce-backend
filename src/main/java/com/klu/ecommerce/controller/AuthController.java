cdf1b737ccc10f164926182447aa686c8f3f60ad
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-10-14 20:37:24 +0530

    Add signup endpoint in AuthController

--- src/main/java/com/klu/ecommerce/controller/AuthController.java
+++ src/main/java/com/klu/ecommerce/controller/AuthController.java
@@ -13,6 +13,7 @@ public class AuthController {
     public AuthController(UserService userService) {
         this.userService = userService;
     }
+
     
     @PostMapping("/signup")
     public ResponseEntity<String> registerUser(@RequestBody Map<String, String> request) {
