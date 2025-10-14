98ff893c9a54692c6c1a20bca048a087552b3068
krHimanshu123 <162711386+krHimanshu123@users.noreply.github.com>
2025-10-14 20:38:26 +0530

    Add category field with getter and setter methods

--- src/main/java/com/klu/ecommerce/entity/Product.java
+++ src/main/java/com/klu/ecommerce/entity/Product.java
@@ -19,6 +19,7 @@ public class Product {
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
+    
     public String getCategory() { return category; }
     public void setCategory(String category) { this.category = category; }
 
